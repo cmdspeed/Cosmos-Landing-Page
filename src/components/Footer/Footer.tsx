4b82706a0d592fdb88e280c02a3f8ba1a8dfdbfd
cmdspeed <cmdspeed@gmail.com>
2024-09-30 14:57:53 +0200

    add logo and edit px to rem in Logo componet

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "../Logo/Logo";
+
+export const Footer = () => {
+  return (
+    <div>
+      <Logo width={9.61} height={1.5} />
+    </div>
+  );
+};
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,11 @@
 import Odyssey from "../../assets/images/Odyssey.webp";
 
-export const Logo = () => {
-  return <img src={Odyssey} width={128.13} height={20} alt="Cosmos Logo" />;
+export const Logo = ({ width = 8, height = 1.25 }) => {
+  return (
+    <img
+      src={Odyssey}
+      style={{ width: `${width}rem`, height: `${height}rem` }}
+      alt="Cosmos Logo"
+    />
+  );
 };
