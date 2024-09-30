7037c1e002a63d057a6e77a07d7d0604ba42b017
cmdspeed <cmdspeed@gmail.com>
2024-09-30 16:01:30 +0200

    add tablet and pc view values

--- src/App.styled.js
+++ src/App.styled.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
-import { mobileView } from "./helpers/resposive-views";
+import { mobileView, pcView, tabletView } from "./helpers/resposive-views";
 export const HomeContainer = styled.div`
   margin: 1.62rem auto;
   max-width: ${mobileView};
+  @media (min-width: ${tabletView}) {
+    max-width: ${tabletView};
+  }
+  @media (min-width: ${pcView}) {
+    max-width: ${pcView};
+  }
 `;
--- src/helpers/resposive-views.js
+++ src/helpers/resposive-views.js
@@ -1,3 +1,11 @@
 export const mobileView = () => {
   return 375 + "px";
 };
+
+export const tabletView = () => {
+  return 1024 + "px";
+};
+
+export const pcView = () => {
+  return 1920 + "px";
+};
